0cfd23b909511933d615583e767715f033e950a1
NatnichaN <natnicha@finnmobile.io>
2023-06-29 13:43:38 +0700

    remove unused code

--- btc-app/CurrencyViewController.swift
+++ btc-app/CurrencyViewController.swift
@@ -16,16 +16,34 @@ class CurrencyViewController {
     
     var delegate: CurrencyViewControllerDelegate?
     var currency: Currency?
-    init() {}
+    private var pollingTimer: Timer!
+    let fetchInterval = 60.0
     
-    func fetchData(){
+    init() {
+    }
+    
+    func initFetchScheduled() {
+        pollingTimer = Timer.scheduledTimer(timeInterval: fetchInterval, target: self, selector: #selector(fetchData), userInfo: nil, repeats: true)
+    }
+    
+    @objc func fetchData() {
         APIManger.shared.coinDesk(success: { [weak self] (currency) in
-            guard let strongSelf = self else {
-                return
-            }
-            strongSelf.currency = currency
+            guard let weakSelf = self else { return }
+            weakSelf.currency = currency
+            print("lasted update time:  \(currency.contentTimeStamp.updated)")
+            weakSelf.delegate?.fetchContentSucces(vc: weakSelf)
+            guard weakSelf.pollingTimer == nil else { return }
+            weakSelf.initFetchScheduled()
         }, failure: { [weak self] error in
             print("\(error)")
+            guard let weakSelf = self else { return }
+            guard weakSelf.pollingTimer != nil else { return }
+            weakSelf.resetTimer()
         })
+    }
+    
+    func resetTimer() {
+        pollingTimer?.invalidate()
+        pollingTimer = nil
     }
 }
--- btc-app/ViewController.swift
+++ btc-app/ViewController.swift
@@ -6,53 +6,24 @@
 //
 
 import UIKit
-//protocol ViewControllerDelegate: AnyObject {
-//    func reloadContentSucces(vc: CurrencyViewController)
-//    func reloadContentFailed(vc: CurrencyViewController)
-//}
-
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CurrencyViewControllerDelegate {
+class ViewController: UIViewController, CurrencyViewControllerDelegate {
 
     var currencyVC = CurrencyViewController()
-    private var refreshControl: UIRefreshControl!
-    @IBOutlet weak var tableView: UITableView!
-    var model =  ["1", "2", "3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        refreshControl = UIRefreshControl()
-        refreshControl.tintColor = UIColor.blue
-        refreshControl.addTarget(self, action: #selector(self.didPullToRefresh(control:)), for: .valueChanged)
-        tableView.refreshControl = refreshControl
         currencyVC.delegate = self
         currencyVC.fetchData()
     }
-    
-    @objc func didPullToRefresh(control: UIRefreshControl) {
-        currencyVC.fetchData()
-    }
-    
-    // MARK: - UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.model.count
-    }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "title \(String(describing: self.model[indexPath.row]))"
-        return cell
-    }
-    // MARK: - UITableViewDelegate
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80.0
-    }
     // MARK: - CurrencyViewControllerDelegate
     func fetchContentSucces(vc: CurrencyViewController) {
-        tableView.reloadData()
+        print("fetchContentSucces")
     }
     
     func fetchContentFailed(vc: CurrencyViewController) {
-        print("reloadContentFailed")
+        print("fetchContentFailed")
     }
 }
 
